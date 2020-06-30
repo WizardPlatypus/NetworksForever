f45b72cdac84191a626eac058ffb916693e083ea
= <=>
2020-06-30 15:54:46 +0300

    At that time i have to choose between array of knots pointers and just array of knots. Second posibility looks easier, so i prefer it

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # NetworksForever
-It's my Course Project repository, where i'm going to solve some networks (graphs) problems.
+It's my Course Project repository, where i'm going to solve one interesting network problem, which could be called "Least time to go around all cities in the country by several riders"
--- knot.h
+++ knot.h
@@ -1,22 +1,22 @@
 class knot
 {
 private:
-   int values[];
+   int* values;
    int values_number;
-   knot *knots[];
+   knot* knots;
    int knots_number;
 public:
    knot(int *values, int size){
-      *this->values = *new int[size];
+      this->values = new int[size];
       for(int i = 0; i < size; ++i){
          this->values[i] = values[i];
       }
-      *this->knots = nullptr;
+      knots = nullptr;
    };
    ~knot(){
       delete[] values;
       for(int i = 0; i < knots_number; ++i){
-         this->knots[i]->~knot();
+         knots[i].~knot();
       }
       delete[] knots;
    };
