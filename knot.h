0eddb1fa5018e6187fd58e00b3f0f195acd0d14f
= <=>
2020-06-30 12:52:42 +0300

    Added a new class called Knot which will be a part of stack-tree data class

---
+++ knot.h
@@ -0,0 +1,23 @@
+class knot
+{
+private:
+   int values[];
+   int values_number;
+   knot *knots[];
+   int knots_number;
+public:
+   knot(int *values, int size){
+      *this->values = *new int[size];
+      for(int i = 0; i < size; ++i){
+         this->values[i] = values[i];
+      }
+      *this->knots = nullptr;
+   };
+   ~knot(){
+      delete[] values;
+      for(int i = 0; i < knots_number; ++i){
+         this->knots[i]->~knot();
+      }
+      delete[] knots;
+   };
+};
