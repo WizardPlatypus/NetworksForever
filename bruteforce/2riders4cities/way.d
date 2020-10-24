5c88b1e1de982e7c1c654c8a8d15b6f1abb6e947
= <=>
2020-10-24 15:45:32 +0300

    Changing Way.bestWay and Way.PointInTime.next from pointers to arrays of pointers. Providing safety in destruction of Way.PointInTime. Documenting .dup property, and why in end of function i setting some pointers to null. No need to add globalTime as part of Way.PointInTime structure. In the end i will set tail's uint fields to the value of globalTime — and compare those fields with fields of bestWays. Have done search function. Algorithm:     1. Saving where we are, incrementing globalTime to time needed to     move riders.     2. Checking which riders arrived to their targets, modyfying     matrix and visits array.     3. For each rider who arrived checking whether he can go somewhere.     4. If someone can go somewhere — then search all posibilites.     5. If no one can go anywhere, or all nodes are visited, then saving     where we are and checking whether our way better or equal to     bestWays.
    TODO:
        1. Clear branches — delete files which you don't need.
        2. Do unittesting on branch fullTree.
        3. Test whole class.
    
    while true:
        doNotGiveUp();
        doNotGiveIn();
        beClever();
        fixBugs();

--- bruteforce/2riders4cities/way.d
+++ bruteforce/2riders4cities/way.d
@@ -18,9 +18,15 @@ class Way
         uint time1;
         /** Second rider's time to arrive in target node: **/
         uint time2;
+        
+        /**
+        No need to add globalTime as part of PointInTime structure.
+        In the end i will set tail's uint fields to the value of
+        globalTime — and compare those fields with fields of bestWays.
+        **/
 
         /** Next point in time: **/
-        PointInTime* next;
+        PointInTime*[] next;
         /** Previous point in time: **/
         PointInTime* prev;
 
@@ -28,7 +34,7 @@ class Way
         Constructs new point in time using targets of 2 riders.
         **/
         this(uint target1, uint target2, uint time1 = 0, uint time2 = 0,
-            PointInTime* next = null, PointInTime* prev = null)
+            PointInTime*[] next = null, PointInTime* prev = null)
         {
             this.target1 = target1;
             this.target2 = target2;
@@ -36,6 +42,27 @@ class Way
             this.time2 = time2;
             this.next = next;
             this.prev = prev;
+        }
+        /**
+        Destructor: destroys every child node, and considers that
+            parent is safe.
+        **/
+        ~this()
+        {
+            for (uint i = 0; i < next.length; ++i)
+            {
+                destroy(*next);
+            }
+            next = [];
+
+            if (prev == null)
+                return;
+            uint index = 0;
+            for (; index < (*prev).next; ++index)
+                if ((*prev).next[index] == this)
+                    break;
+            (*prev).next = (*prev).next[0 .. index] ~
+                (*prev).next[index + 1 .. $];
         }
     }
 
@@ -58,7 +85,7 @@ class Way
     First node of linked list of points in time, which represents
         best way to visit all nodes in minimal time.
     **/
-    PointInTime* bestWay;
+    PointInTime*[] bestWays;
     
     /**
     Constructor, which starts searching of best way.
@@ -68,11 +95,12 @@ class Way
     this(uint origin, uint[][] matrix)
     {
         this.matrix = matrix;
+        this.bestWays = [];
 
         bool[cityNumber] visits = false;
         visits[origin] = true;
         
-        PointInTime* begin = new PointInTime(origin, origin);
+        PointInTime* now = new PointInTime(origin, origin);
 
         for (uint i = 0; i < cityNumber; ++i)
         {
@@ -80,10 +108,14 @@ class Way
             {
                 if (matrix[i][j] == 0)
                     continue;
+                /+ {object}.dup — property to create a copy of an {object} +/
                 search(i, j, matrix[origin][i], matrix[origin][j],
-                    matrix, visits, begin, 0);
+                    matrix.dup, visits.dup, now, 0);
             }
         }
+
+        // to do not destroy object on which 'now' has reference.
+        now = null;
     }
     /**
     Constructor, which starts searching of best way.
@@ -113,7 +145,7 @@ class Way
     }
     /**
     Moves riders on time, needed to one or both of them to arrive to
-        it's/their target. Returns that time.
+        its/their target/s. Returns that time.
     **/
     uint move(ref uint time1, ref uint time2)
     {
@@ -132,6 +164,21 @@ class Way
                 return false;
         return true;
     }
+
+    /**
+    Looks wether there are ways from specified node.
+    **/
+    bool weCanGoFurth(const uint from, const uint[cityNumber][cityNumber] matrix)
+    {
+        uint count = 0;
+        for (uint i = 0; i < cityNumber; ++i)
+            if (matrix[from][i] != 0)
+                ++count;
+        if (count == 0)
+            return false;
+        else
+            return true;
+    }
     /**
     Search for best way.
     **/
@@ -141,7 +188,7 @@ class Way
     {
         PointInTime* now = new PointInTime(target1, target2, time1,
             time2, null, then);
-        then.next = now;
+        then.next ~= now;
         globalTime += move(time1, time2);
 
         if (time1 == 0 && time2 == 0)
@@ -152,18 +199,64 @@ class Way
             visits[now.target1] = true;
             visits[now.target2] = true;
 
-            for (uint i = 0; i < cityNumber; ++i)
+            if (areAllVisited(visits)) // in case when all nodes are visited
             {
-                if (matrix[now.target1][i] == 0)
-                        continue;
-                for (uint j = 0; j < cityNumber; ++j)
+                PointInTime* end = new PointInTime(globalTime, globalTime,
+                    globalTime, globalTime, null, now);
+                (*now).next ~= end;
+                if (globalTime < bestWays[0].time1)
+                    bestWays = [end];
+                else if (globalTime == bestWays[0].time1)
+                    bestWays ~= end;
+                end = null;
+
+                return;
+            }
+            // true wether first rider can go anywhere
+            immutable bool first = weCanGoFurth(now.target1, matrix);
+            // true wether second rider can go anywhere
+            immutable bool second = weCanGoFurth(now.target2, matrix);
+
+            if (first && second) //if both can go somewhere
+            {
+                for (uint i = 0; i < cityNumber; ++i)
                 {
-                    if (matrix[now.target2][j] == 0)
+                    if (matrix[now.target1][i] == 0)
                         continue;
-                    search(i, j, matrix[now.target1][i],
-                        matrix[now.target2][j], matrix.dup,
-                        visits.dup, now, globalTime);
+                    for (uint j = 0; j < cityNumber; ++j)
+                    {
+                        if (matrix[now.target2][j] == 0)
+                            continue;
+                        search(i, j, matrix[now.target1][i],
+                            matrix[now.target2][j], matrix.dup,
+                            visits.dup, *now, globalTime);
+                    }
                 }
+            }
+            else if (first) // if only first can go somewhere
+            {
+                for (uint i = 0; i < cityNumber; ++i)
+                {
+                    if (matrix[now.target1][i] == 0)
+                        continue;
+                    search(i, target2, matrix[now.target1][i], time2,
+                        matrix.dup, visits.dup);
+                }
+            }
+            else if (second) // if only first can go somewhere
+            {
+                for (uint i = 0; i < cityNumber; ++i)
+                {
+                    if (matrix[now.target2][i] == 0)
+                        continue;
+                    search(target1, i, time1, matrix[target2][i],
+                        matrix.dup, visits.dup);
+                }
+            }
+            else // if no one can go anywhere
+            {
+                destroy(*now);
+                return;
             }
         }
         else
@@ -174,28 +267,33 @@ class Way
                 matrix[then.target1][now.target1] = 0;
                 visits[now.target1] = true;
 
-                for (uint i = 0; i < cityNumber; ++i)
-                {
-                    if (matrix[now.target1][i] == 0)
-                        continue;
-                    search(i, target2, matrix[now.target1][i], time2,
-                        matrix.dup, visits.dup, now, globalTime);
-                }
+                if (weCanGoFurth(target1, matrix))
+                    for (uint i = 0; i < cityNumber; ++i)
+                    {
+                        if (matrix[now.target1][i] == 0)
+                            continue;
+                        search(i, target2, matrix[now.target1][i], time2,
+                            matrix.dup, visits.dup, *now, globalTime);
+                    }
             }
             else if (time2 == 0)
             {
                 matrix[then.target2][now.target2] = 0;
                 visits[now.target2] = true;
 
-                for (uint i = 0; i < cityNumber; ++i)
-                {
-                    if (matrix[now.target2][i] == 0)
-                        continue;
-                    search(target1, i, time1, matrix[now.target2][i],
-                        matrix.dup, visits.dup, now, globalTime);
-                }
+                if (weCanGoFurth(target1, matrix))
+                    for (uint i = 0; i < cityNumber; ++i)
+                    {
+                        if (matrix[now.target2][i] == 0)
+                            continue;
+                        search(target1, i, time1, matrix[now.target2][i],
+                            matrix.dup, visits.dup, *now, globalTime);
+                    }
             }
         }
 
+        // to do not destroy objects to which 'then' and 'now' have reference.
+        then = null;
+        now = null;
     }
 }
