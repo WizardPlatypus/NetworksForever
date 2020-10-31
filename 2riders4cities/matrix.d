84634645d4ce940aa3bf2031899fc5f9fe0569bf
WizardPlatypus <f.volik.m@gmail.com>
2020-11-01 00:06:02 +0200

    Створив файл matrix.d : він відповідає за матрицю суміжності графа та роботу з нею.
    TODO:
    	1. Зкомпілювати файл та перевірити unittestʼи.
    	2. Почати роботу над файлом tree.d(moment.d).

---
+++ 2riders4cities/matrix.d
@@ -0,0 +1,106 @@
+module matrix;
+
+// константи
+import consts : citiesNumber, ridersNumber;
+
+// дуга між двома містами
+struct Curve
+{
+	private:
+
+	// час, який необхідно витратити щоб пройти цю дугу
+	uint time;
+	// массив відвідувань цієї дуги вершниками 
+	bool[ridersNumber] checks;
+
+	public:
+
+	// конструктор
+	this(uint time)
+	{
+		this.time = time;
+		checks = new bool[ridersNumber]
+	}
+
+	// встановлює новий час для подолання цієї дуги
+	void setTime(uint time)
+	{
+		this.time = time;
+	}
+
+	// відмічає вершника за індексом id
+	void markId(uint id)
+	in
+	{
+		assert(id < ridersNumber);
+	}
+	do
+	{
+		this.checks[id] = true;
+	}
+
+	// 
+	bool check(uint id)
+	in
+	{
+		assert(id < ridersNumber);
+	}
+	do
+	{
+		return checks[id];
+	}
+}
+
+// Матриця відстаней
+struct Matrix
+{
+	// Матриця дуг
+	// matrix[i][j] - дуга з міста i в місто j
+	Curve[citiesNumber][citiesNumber] matrix;
+
+	// конструктор
+	this(uint[][] matrix)
+	in
+	{
+		assert(matrix.length == citiesNumber)
+		
+		for (uint i = 0; i < matrix.length; ++i)
+			assert(matrix[i].length == citiesNumber);
+	}
+	do
+	{
+		this.matrix = new Curve[citiesNumber][citiesNumber];
+		for (uint i = 0; i < citiesNumber; ++i)
+			for (uint j = 0; j < citiesNumber; ++j)
+				this.matrix[i][j].setTime(matrix[i][j]);
+	}
+
+	// відмітити дугу з i в j як пройдену вершником з індексом id
+	void visit(uint i, uint j, uint id)
+	in
+	{
+		assert(i < citiesNumber);
+		assert(j < citiesNumber);
+		assert(id < ridersNumber);
+	}
+	do
+	{
+		this.matrix[i][j].markId(id);
+	}
+
+	// 
+	uint[] possibleTargets(uint from, uint id)
+	in
+	{
+		assert(from < citiesNumber);
+		assert(id < ridersNumber);
+	}
+	do
+	{
+		uint[] targets = new uint[];
+		for (uint i = 0; i < ridersNumber; ++i)
+			if (!matrix[from][i].check(id))
+				targets ~= i;
+		return targets;		
+	}
+}
