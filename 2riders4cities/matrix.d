982ef8d8867ae34420741c89b930ee74a30e90db
= <=>
2020-11-03 00:37:03 +0200

    Створюю unittest'и, потрохи перероблюю проограму.

--- 2riders4cities/matrix.d
+++ 2riders4cities/matrix.d
@@ -10,28 +10,24 @@ struct Curve
 {
 	private:
 
-	// час, який необхідно витратити щоб пройти цю дугу
-	uint time;
 	// массив відвідувань цієї дуги вершниками 
 	bool[ridersNumber] checks;
 
 	public:
 
+	// час, який необхідно витратити щоб пройти цю дугу
+	uint time;
+
 	// конструктор
 	this(uint time)
 	{
 		this.time = time;
-		checks = new bool[ridersNumber]
-	}
-
-	// встановлює новий час для подолання цієї дуги
-	void setTime(uint time)
-	{
-		this.time = time;
+		checks = new bool[ridersNumber];
+		checks[] = false;
 	}
 
 	// відмічає вершника за індексом id
-	void markId(uint id)
+	void mark(uint id)
 	in
 	{
 		assert(id < ridersNumber);
@@ -41,7 +37,7 @@ struct Curve
 		this.checks[id] = true;
 	}
 
-	// 
+	// повертає стан вершника за id 
 	bool check(uint id)
 	in
 	{
@@ -51,6 +47,17 @@ struct Curve
 	{
 		return checks[id];
 	}
+
+	unittest
+	{
+		Curve c = Curve(3);
+		assert(c.time == 3);
+		for (uint i = 0; i < ridersNumber; ++i)
+			assert(c.check(i) == false);
+		c.mark(0);
+		assert(c.check(0) == true);
+	}
+	
 }
 
 public:
@@ -77,7 +84,6 @@ struct Matrix
 		this.matrix = matrix.dup;
 	}
 
-
 	public:
 
 	// конструктор за матрицею відстаней
@@ -94,7 +100,7 @@ struct Matrix
 		this.matrix = new Curve[citiesNumber][citiesNumber];
 		for (uint i = 0; i < citiesNumber; ++i)
 			for (uint j = 0; j < citiesNumber; ++j)
-				this.matrix[i][j].setTime(matrix[i][j]);
+				this.matrix[i][j] = Curve(matrix[i][j]);
 	}
 
 	// відмітити дугу з i в j як пройдену вершником з індексом id
@@ -107,8 +113,23 @@ struct Matrix
 	}
 	do
 	{
-		this.matrix[i][j].markId(id);
+		this.matrix[i][j].mark(id);
 	}
+
+	unittest
+	{
+		auto m = Matrix([
+			[ 1, 1, 1, 1, 1],
+			[ 1, 1, 1, 1, 1],
+			[ 1, 1, 1, 1, 1],
+			[ 1, 1, 1, 1, 1],
+			[ 1, 1, 1, 1, 1]
+		]);
+		uint i = 0;
+		uint j = 0;
+		uint id = 0;
+		m.visit(i, j, id);
+		assert(m[i][j].check(id));
 
 	// можливі цілі для руху з вершини from для вершника id
 	uint[] possibleTargets(uint from, uint id)
@@ -125,6 +146,17 @@ struct Matrix
 				targets ~= i;
 		return targets;		
 	}
+
+	unittest
+	{
+		auto m = Matrix([
+			[ 1, 1, 1, 1, 1],
+			[ 1, 1, 1, 1, 1],
+			[ 1, 1, 1, 1, 1],
+			[ 1, 1, 1, 1, 1],
+			[ 1, 1, 1, 1, 1]
+		]);
+		uint from = 0;
 
 	// повертає копію даного об'єкта
 	Matrix dup()
